d44ad720e1d3b9a707bea62c298addc1cfe3a913
Qoslaye <iamqoslaye@gmail.com>
2024-12-01 10:16:53 +0300

    Flask-WTF Example Project : first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from forms import ContactForm
+app = Flask(__name__)  
+
+app.secret_key = 'development key'  
+  
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/contact', methods = ['GET', 'POST'])  
+def contact():  
+    
+    form=ContactForm()
+
+    if form.validate_on_submit():
+        return ' form submitted'
+    return render_template('contact.html', form=form)
+    
+   
+  
+  
+
+if __name__ == '__main__':  
+   app.run(debug = True)  
